382f288bdde677a8985efd2c236d270444b439dd
Ablo224 <ablobalde92@gmail.com>
2020-03-06 22:10:21 +0100

    implémentation de la classe Employes qui sera la classe mere de la classe Vendeur

--- pglp_3.2/src/main/java/uvocp606235/Employes.java
+++ pglp_3.2/src/main/java/uvocp606235/Employes.java
@@ -1,11 +1,18 @@
 package uvocp606235;
 
 public class Employes implements Salaire{
-
+	
+	private int nbY;
+	
+	public Employes(int nbY)
+	{
+		this.nbY = nbY;
+	}
+	
 	@Override
 	public double calculsalaire() {
 		// TODO Auto-generated method stub
-		return 0;
+		return 1500 + nbY*20;
 	}
 
 	
--- pglp_3.2/src/main/java/uvocp606235/Vendeur.java
+++ pglp_3.2/src/main/java/uvocp606235/Vendeur.java
@@ -1,5 +1,17 @@
 package uvocp606235;
 
 public class Vendeur extends Employes{
+	
+	public Vendeur(int nbY) {
+		super(nbY);
+		// TODO Auto-generated constructor stub
+	}
+
+	private int commission;
+	
+	public double salaireVendeur()
+	{
+		return super.calculsalaire() + commission;
+	}
 
 }
