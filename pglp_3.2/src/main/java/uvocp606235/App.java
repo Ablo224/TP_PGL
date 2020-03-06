eeb8efc99eda7b28fe2d34a722c226187fd4ab74
Ablo224 <ablobalde92@gmail.com>
2020-03-06 23:04:20 +0100

    ajout et implémention de la classe manager

--- pglp_3.2/src/main/java/uvocp606235/App.java
+++ pglp_3.2/src/main/java/uvocp606235/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Vendeur v1 = new Vendeur(5,100);
-		System.out.println("Salaire vendeur: " +v1.salaireVendeur());
+		System.out.println("Salaire vendeur: " +v1.calculsalaire());
 		
 		Employes e1 = new Employes(2);
 		System.out.println("Salaire emplyé simple: " +e1.calculsalaire());
